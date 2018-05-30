c78a5a50068d07e536f6e2e5c22c155771c5ec73
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-05-29 22:04:31 -0700

    Add GH Pages
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- rxads/src/androidTest/java/com/shaishavgandhi/rxads/RxAdLoaderTest.kt
+++
@@ -1,7 +0,0 @@
-package com.shaishavgandhi.rxads
-
-/**
- * Created by shaishav.gandhi on 1/31/18.
- */
-class RxAdLoaderTest {
-}
--- rxads/src/main/java/com/shaishavgandhi/rxads/error/AdRequestErrorException.kt
+++
@@ -1,7 +0,0 @@
-package com.shaishavgandhi.rxads.error;
-
-/**
- * Created by shaishav.gandhi on 1/13/18.
- */
-
-class AdRequestErrorException(adRequestError: AdRequestError) : Exception(adRequestError.getMessage())
